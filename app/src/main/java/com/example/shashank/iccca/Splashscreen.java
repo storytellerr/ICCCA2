fb32a06a339af11ffe98df1703ff06b3e3693101
storyteller <shashanksharma580S@gmail.com>
2017-01-25 13:54:46 +0530

    19/01/2017

--- app/src/main/java/com/example/shashank/iccca/Splashscreen.java
+++ app/src/main/java/com/example/shashank/iccca/Splashscreen.java
@@ -1,18 +1,36 @@
 package com.example.shashank.iccca;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.romainpiel.shimmer.Shimmer;
+import com.romainpiel.shimmer.ShimmerTextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splashscreen extends AppCompatActivity {
 
 
+    ShimmerTextView tv,tv1;
+    Shimmer shimmer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        Intent in = new Intent(Splashscreen.this,MainActivity.class);
-        startActivity(in);
+        tv = (ShimmerTextView) findViewById(R.id.shimmer_tv);
+        tv1=(ShimmerTextView) findViewById(R.id.shimmer_tv1);
+        shimmer = new Shimmer();
+        shimmer.start(tv);
+        shimmer.start(tv1);
+
+        new Timer().schedule(new TimerTask(){
+            public void run() {
+                startActivity(new Intent(Splashscreen.this,MainActivity.class));
+                finish();
+            }
+        }, 5000);
 
     }
 }
