42cfbf14c0defd11ce238ad850794c961a9c5a3e
storyteller <shashanksharma580S@gmail.com>
2017-02-20 20:28:57 +0530

    19/01/2017

--- app/src/main/java/com/example/shashank/iccca/MainActivity.java
+++ app/src/main/java/com/example/shashank/iccca/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity
 
     {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
